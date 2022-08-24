c776e9d53efc96d0c8073c364baa11559809f416
vbloher <vbloher7@gmail.com>
2022-08-24 20:54:56 +0700

    add Total staked component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Achievements from "Components/Achievements"
 import DvsBlock from "Components/DvsBlock"
 import Greeting from "Components/Greeting"
 import { mainnetChains } from "Data"
+import Typography from "@mui/material/Typography"
 
 export default function App() {
   return (
@@ -20,6 +21,25 @@ export default function App() {
       <Greeting />
 
       <Container maxWidth="lg">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          width="100%"
+          paddingY={3}
+          bgcolor="#FBFBFA"
+        >
+          <Box
+            component={Typography}
+            variant="h4"
+            textAlign="center"
+            paddingBottom={2}
+          >
+            Total amount of staked assets with vbloher:
+          </Box>
+          <Typography variant="h5">300,000$</Typography>
+        </Box>
+
         <ChainGrid
           title="Mainnets"
           subtitle="You can delegate your tokens to our trusted validators in Mainnet."
