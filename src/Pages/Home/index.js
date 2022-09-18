0347ba888e9728722baea7ab6261b5c67fcb2608
vbloher <vbloher7@gmail.com>
2022-09-19 00:03:41 +0700

    simplify Home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,22 +2,50 @@ import * as React from "react"
 import MainLayout from "Components/MainLayout"
 import Greeting from "Components/Greeting"
 import Container from "@mui/material/Container"
-import ChainGrid from "Components/ChainGrid"
 import { mainnetChains } from "Services/data"
 import Achievements from "Components/Achievements"
 import StakedFundsCounter from "Components/StakedCounter"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import Grid from "@mui/material/Grid"
+import ChainCard from "Components/ChainCard"
 
 const Home = () => (
   <MainLayout>
     <Greeting />
     <StakedFundsCounter />
+
     <Container maxWidth="lg">
-      <ChainGrid
-        title="Mainnets"
-        subtitle="You can delegate your tokens to our trusted validators in Mainnet."
-        chains={mainnetChains}
-      />
+      <Box>
+        <Box
+          paddingTop={2}
+          paddingBottom={3}
+        >
+          <Typography variant="h3">Networks</Typography>
+          <Typography variant="h5">
+            You can delegate your tokens with auto compound feature with my validators
+          </Typography>
+        </Box>
+        <Grid
+          container
+          spacing={3}
+        >
+          {mainnetChains.map((chain) => (
+            <Grid
+              key={chain.name}
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+            >
+              <ChainCard chain={chain} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Container>
+
     <Achievements />
   </MainLayout>
 )
