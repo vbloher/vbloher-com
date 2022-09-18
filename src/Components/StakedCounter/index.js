2d1147f2ee660bfef7864f9637986b6d9d582f57
vbloher <vbloher7@gmail.com>
2022-09-19 00:40:04 +0700

    add tooltip to StakedCounter

--- src/Components/StakedCounter/index.js
+++ src/Components/StakedCounter/index.js
@@ -3,10 +3,27 @@ import Typography from "@mui/material/Typography"
 import * as React from "react"
 import { useEffect, useState } from "react"
 import getTotalStaked from "Services/api"
-import { CircularProgress } from "@mui/material"
+import CircularProgress from "@mui/material/CircularProgress"
+import Tooltip from "@mui/material/Tooltip"
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline"
+import { makeStyles } from "@mui/styles"
+import IconButton from "@mui/material/IconButton"
+
+const useStyles = makeStyles((theme) => ({
+  helpIcon: {
+    marginLeft: theme.spacing(1),
+    opacity: 0.3,
+    transition: theme.transitions.create("opacity"),
+
+    "&:hover": {
+      opacity: 1,
+    },
+  },
+}))
 
 const StakedFundsCounter = () => {
   const [totalStaked, setTotalStaked] = useState(null)
+  const classes = useStyles()
 
   const stakedComponent =
     totalStaked !== null ? (
@@ -35,12 +52,21 @@ const StakedFundsCounter = () => {
       <Box
         component={Typography}
         variant="h4"
-        textAlign="center"
         paddingBottom={2}
       >
         Total amount of staked assets with vbloher:
       </Box>
-      {stakedComponent}
+      <Box
+        display="flex"
+        alignItems="center"
+      >
+        {stakedComponent}
+        <Tooltip title="*prices by CoinGecko">
+          <IconButton classesName={classes.helpIcon}>
+            <HelpOutlineIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </Box>
   )
 }
