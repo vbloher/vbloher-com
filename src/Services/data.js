c4e9ef75bdf90d4f08638cdaef1a031dfa57f27b
vbloher <vbloher7@gmail.com>
2023-03-06 20:07:32 +0700

    update chains

--- src/Services/data.js
+++ src/Services/data.js
@@ -17,13 +17,6 @@ export const mainnetChains = [
     exponent: 6,
   },
   {
-    name: "Galaxy",
-    registryName: "galaxy",
-    valoper: "galaxyvaloper173mgkdad6f935ys7gd2yfj6z47khkwy6f9ce7n",
-    exponent: 6,
-    apr: 156,
-  },
-  {
     name: "Kujira",
     registryName: "kujira",
     valoper: "kujiravaloper1yafsywsluqdvlhtp30tcdhw496vg6ey3zs4u84",
@@ -45,8 +38,9 @@ export const mainnetChains = [
   },
   {
     name: "Mises",
+    registryName: "miese",
     valoper: "misesvaloper1uwkdx83hxkd46ak0tc4npq3hj50p9xpvx9424s",
-    registryName: undefined,
     stakeLink: "https://gw.mises.site/validators/misesvaloper1uwkdx83hxkd46ak0tc4npq3hj50p9xpvx9424s",
+    exponent: 6,
   },
 ]
